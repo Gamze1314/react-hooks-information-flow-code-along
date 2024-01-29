cb8a92a82bc64700f139c60cb094f396eb4cb21a
gamze <gamzekandara@gmail.com>
2024-01-28 21:49:01 -0500

    adds App.js

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor  , color }) {
+ // onChangeColor : is a callback function passed as prop to update the state in Parent component
+   function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor);
+  }
+
+  console.log(onChangeColor)
+  return <div onClick={handleClick} className="child" style={{ backgroundColor: color }} />;
 }
 
 export default Child;
